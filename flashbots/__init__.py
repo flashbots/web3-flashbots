ed951a1e4e0cd33d1620da25824b41ac25f4374d
itsdeka <dario.moceri@bitfinex.com>
2021-07-30 10:11:22 +0200

    Added support for custom endpoint_uri

--- flashbots/__init__.py
+++ flashbots/__init__.py
@@ -8,6 +8,7 @@ from web3._utils.module import attach_modules
 from .middleware import construct_flashbots_middleware
 from .flashbots import Flashbots
 from .provider import FlashbotProvider
+from eth_typing import URI
 
 DEFAULT_FLASHBOTS_RELAY = "https://relay.flashbots.net"
 
@@ -15,12 +16,13 @@ DEFAULT_FLASHBOTS_RELAY = "https://relay.flashbots.net"
 def flashbot(
     w3: Web3,
     signature_account: LocalAccount,
+    endpoint_uri: Optional[Union[URI, str]] = None
 ):
     """
     Injects the flashbots module and middleware to w3.
     """
 
-    flashbots_provider = FlashbotProvider(signature_account)
+    flashbots_provider = FlashbotProvider(signature_account, endpoint_uri)
     flash_middleware = construct_flashbots_middleware(flashbots_provider)
     w3.middleware_onion.add(flash_middleware)
 
