d9bb2ea959525dae59a94cbb3ecf0612cee40e82
brock <2791467+zeroXbrock@users.noreply.github.com>
2022-05-16 19:57:25 -0700

    Rework example and readme (#55)
    * make example and readme easier to understand
    
    * update PKG-info & tweak README
    
    * bump patch version (1.0.1)

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # web3-flashbots
 
-This library works by injecting flashbots as a new module in the Web3.py instance, which allows
-submitting "bundles" of transactions directly to miners. This is done by also creating
-a middleware which captures calls to `eth_sendBundle` and `eth_callBundle`, and sends
-them to an RPC endpoint which you have specified, which corresponds to `mev-geth`.
+This library works by injecting flashbots as a new module in the Web3.py instance, which allows submitting "bundles" of transactions directly to miners. This is done by also creating a middleware which captures calls to `eth_sendBundle` and `eth_callBundle`, and sends them to an RPC endpoint which you have specified, which corresponds to `mev-geth`.
 
-To apply correct headers we use FlashbotProvider which injects the correct header on post.
+To apply correct headers we use the `flashbot` method which injects the correct header on POST.
 
-## Example
+## Quickstart
 
 ```python
 from eth_account.signers.local import LocalAccount
@@ -23,15 +20,17 @@ w3 = Web3(HTTPProvider("http://localhost:8545"))
 flashbot(w3, ETH_ACCOUNT_SIGNATURE)
 ```
 
-Now the `w3.flashbots.sendBundle` method should be available to you. Look in `examples/simple.py` for usage examples
+Now the `w3.flashbots.sendBundle` method should be available to you. Look in [examples/simple.py](./examples/simple.py) for usage examples.
+
+### Goerli
+
+To use goerli, add the goerli relay RPC to the `flashbot` function arguments.
+
+```python
+flashbot(w3, ETH_ACCOUNT_SIGNATURE, "https://relay-goerli.flashbots.net")
+```
 
 ## Development and testing
-
-Setup and run (mev-)geth with Websocket support:
-
-```sh
-geth --http --http.api eth,net,web3,txpool --syncmode full
-```
 
 Install [poetry](https://python-poetry.org/)
 
@@ -42,6 +41,18 @@ poetry install
 ```
 
 Tips: PyCharm has a poetry plugin
+
+## Simple Goerli Example
+
+See [examples/simple.py](./examples/simple.py) for environment variable definitions.
+
+```sh
+poetry shell
+ETH_SENDER_KEY=<sender_private_key> \
+PROVIDER_URL=https://eth-goerli.alchemyapi.io/v2/<alchemy_key> \
+ETH_SIGNER_KEY=<signer_private_key> \
+python examples/simple.py
+```
 
 ## Linting
 
--- examples/simple.py
+++ examples/simple.py
@@ -1,103 +1,124 @@
-from eth_account.account import Account
-from eth_account.signers.local import LocalAccount
-from web3.middleware import construct_sign_and_send_raw_middleware
+"""
+Minimal viable example of flashbots usage with dynamic fee transactions.
+Sends a bundle of two transactions which transfer some ETH into a random account.
 
-from flashbots import flashbot
-from web3 import Web3, HTTPProvider
-from web3.types import TxParams, Wei
+Environment Variables:
+- ETH_SENDER_KEY: Private key of account which will send the ETH.
+- ETH_SIGNER_KEY: Private key of account which will sign the bundle. 
+    - This account is only used for reputation on flashbots and should be empty.
+- PROVIDER_URL: HTTP JSON-RPC Ethereum provider URL.
+"""
 
 import os
+import secrets
+from eth_account.account import Account
+from eth_account.signers.local import LocalAccount
+from flashbots import flashbot
+from web3 import Web3, HTTPProvider
+from web3.exceptions import TransactionNotFound
+from web3.types import TxParams
 
-"""
-In this example we setup a transaction for 0.1 eth with a gasprice of 1
-From here we will use Flashbots to pass a bundle with the needed content
-"""
-ETH_ACCOUNT_SIGNATURE: LocalAccount = Account.from_key(
-    os.environ.get("ETH_SIGNATURE_KEY")
-)
-ETH_ACCOUNT_FROM: LocalAccount = Account.from_key(os.environ.get("ETH_SIGNATURE_KEY"))
-ETH_ACCOUNT_TO: LocalAccount = Account.from_key(os.environ.get("ETH_SIGNATURE_KEY"))
+# change this to `False` if you want to use mainnet
+USE_GOERLI = True
+CHAIN_ID = 5 if USE_GOERLI else 1
 
-print("Connecting to RPC")
-# Setup w3 and flashbots
-w3 = Web3(HTTPProvider("http://localhost:8545"))
-flashbot(w3, ETH_ACCOUNT_SIGNATURE)
 
-print(
-    f"From account {ETH_ACCOUNT_FROM.address}: {w3.eth.get_balance(ETH_ACCOUNT_FROM.address)}"
-)
-print(
-    f"To account {ETH_ACCOUNT_TO.address}: {w3.eth.get_balance(ETH_ACCOUNT_TO.address)}"
-)
+def env(key: str) -> str:
+    return os.environ.get(key)
 
-# Setting up a transaction with 1 in gasPrice where we are trying to send
-print("Sending request")
-params: TxParams = {
-    "from": ETH_ACCOUNT_FROM.address,
-    "to": ETH_ACCOUNT_TO.address,
-    "value": w3.toWei("1.0", "gwei"),
-    "gasPrice": w3.toWei("1.0", "gwei"),
-    "nonce": w3.eth.get_transaction_count(ETH_ACCOUNT_FROM.address),
-}
 
-try:
-    tx = w3.eth.send_transaction(
-        params,
-    )
-    print("Request sent! Waiting for receipt")
-except ValueError as e:
-    # Skipping if TX already is added and pending
-    if "replacement transaction underpriced" in e.args[0]["message"]:
-        print("Have TX in pool we can use for the example")
+def random_account() -> LocalAccount:
+    key = "0x" + secrets.token_hex(32)
+    return Account.from_key(key)
+
+
+def main() -> None:
+    # account to send the transfer and sign transactions
+    sender: LocalAccount = Account.from_key(env("ETH_SENDER_KEY"))
+    # account to receive the transfer
+    receiverAddress: str = random_account().address
+    # account to sign bundles & establish flashbots reputation
+    # NOTE: this account should not store funds
+    signer: LocalAccount = Account.from_key(env("ETH_SIGNER_KEY"))
+
+    w3 = Web3(HTTPProvider(env("PROVIDER_URL")))
+    if USE_GOERLI:
+        flashbot(w3, signer, "https://relay-goerli.flashbots.net")
     else:
-        raise
+        flashbot(w3, signer)
+
+    print(f"Sender address: {sender.address}")
+    print(f"Receiver address: {receiverAddress}")
+    print(
+        f"Sender account balance: {Web3.fromWei(w3.eth.get_balance(sender.address), 'ether')} ETH"
+    )
+    print(
+        f"Receiver account balance: {Web3.fromWei(w3.eth.get_balance(receiverAddress), 'ether')} ETH"
+    )
+
+    # bundle two EIP-1559 (type 2) transactions, pre-sign one of them
+    # NOTE: chainId is necessary for all EIP-1559 txns
+    # NOTE: nonce is required for signed txns
+
+    nonce = w3.eth.get_transaction_count(sender.address)
+    tx1: TxParams = {
+        "to": receiverAddress,
+        "value": Web3.toWei(0.001, "ether"),
+        "gas": 21000,
+        "maxFeePerGas": Web3.toWei(200, "gwei"),
+        "maxPriorityFeePerGas": Web3.toWei(50, "gwei"),
+        "nonce": nonce,
+        "chainId": CHAIN_ID,
+        "type": 2,
+    }
+    tx1_signed = sender.sign_transaction(tx1)
+
+    tx2: TxParams = {
+        "to": receiverAddress,
+        "value": Web3.toWei(0.001, "ether"),
+        "gas": 21000,
+        "maxFeePerGas": Web3.toWei(200, "gwei"),
+        "maxPriorityFeePerGas": Web3.toWei(50, "gwei"),
+        "nonce": nonce + 1,
+        "chainId": CHAIN_ID,
+        "type": 2,
+    }
+
+    bundle = [
+        {"signed_transaction": tx1_signed.rawTransaction},
+        {"signer": sender, "transaction": tx2},
+    ]
+
+    # keep trying to send bundle until it gets mined
+    while True:
+        block = w3.eth.block_number
+        print(f"Simulating on block {block}")
+        # simulate bundle on current block
+        try:
+            w3.flashbots.simulate(bundle, block)
+            print("Simulation successful.")
+        except Exception as e:
+            print("Simulation error", e)
+            return
+
+        # send bundle targeting next block
+        print(f"Sending bundle targeting block {block+1}")
+        send_result = w3.flashbots.send_bundle(bundle, target_block_number=block + 1)
+        send_result.wait()
+        try:
+            receipts = send_result.receipts()
+            print(f"\nBundle was mined in block {receipts[0].blockNumber}\a")
+            break
+        except TransactionNotFound:
+            print(f"Bundle not found in block {block+1}")
+
+    print(
+        f"Sender account balance: {Web3.fromWei(w3.eth.get_balance(sender.address), 'ether')} ETH"
+    )
+    print(
+        f"Receiver account balance: {Web3.fromWei(w3.eth.get_balance(receiverAddress), 'ether')} ETH"
+    )
 
 
-print("Setting up flashbots request")
-nonce = w3.eth.get_transaction_count(ETH_ACCOUNT_FROM.address)
-bribe = w3.toWei("0.01", "ether")
-
-signed_tx: TxParams = {
-    "to": ETH_ACCOUNT_TO.address,
-    "value": bribe,
-    "nonce": nonce + 1,
-    "gasPrice": 0,
-    "gas": 25000,
-}
-
-signed_transaction = ETH_ACCOUNT_TO.sign_transaction(signed_tx)
-
-bundle = [
-    #  some transaction
-    {
-        "signer": ETH_ACCOUNT_FROM,
-        "transaction": {
-            "to": ETH_ACCOUNT_TO.address,
-            "value": Wei(123),
-            "nonce": nonce,
-            "gasPrice": 0,
-        },
-    },
-    # the bribe
-    {
-        "signed_transaction": signed_transaction.rawTransaction,
-    },
-]
-
-block = w3.eth.block_number
-
-result = w3.flashbots.send_bundle(bundle, target_block_number=w3.eth.blockNumber + 3)
-result.wait()
-receipts = result.receipts()
-block_number = receipts[0].blockNumber
-
-# the miner has received the amount expected
-bal_before = w3.eth.get_balance(ETH_ACCOUNT_FROM.address, block_number - 1)
-bal_after = w3.eth.get_balance(ETH_ACCOUNT_FROM.address, block_number)
-profit = bal_after - bal_before - w3.toWei("2", "ether")  # sub block reward
-print("Balance before", bal_before)
-print("Balance after", bal_after)
-assert profit == bribe
-
-# the tx is successful
-print(w3.eth.get_balance(ETH_ACCOUNT_TO.address))
+if __name__ == "__main__":
+    main()
