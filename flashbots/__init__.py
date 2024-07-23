0336d61033abc86876e6ed04d93e213901db2149
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-07-23 13:21:49 -0700

    remove PoA middleware (prev. used for goerli)

--- flashbots/__init__.py
+++ flashbots/__init__.py
@@ -23,12 +23,6 @@ def flashbot(
     """
 
     flashbots_provider = FlashbotProvider(signature_account, endpoint_uri)
-
-    # TODO: see if we can remove this safely; goerli is deprecated
-    # goerli connection requires extra PoA middleware
-    if endpoint_uri is not None and "goerli" in endpoint_uri:
-        w3.middleware_onion.inject(geth_poa_middleware, layer=0)
-
     flash_middleware = construct_flashbots_middleware(flashbots_provider)
     w3.middleware_onion.add(flash_middleware)
 
